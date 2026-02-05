4b6a57325a16b94c09ef1fb6299fe89f9e9ab135
Kamran Gasimov <92179625+kamrangweb@users.noreply.github.com>
2026-02-05 21:10:35 +0400

    Refactor Footer component structure and content

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,105 +1,58 @@
-import logo from '../image/logo192.png'
+function Footer() {
+  return (
+    <footer id="contact" className="footer">
+      <div className="container">
 
-function Footer(){
+        <div className="footer-header">
+          <h3>Contact us</h3>
+        </div>
 
+        <div className="footer-content">
 
-    return (
-        <footer id="contact" className="footer">
-            <div className="container">
-                <div className="row footer-contact-button margin-bottom-30">
-                    <div className="column footer-h3-p text-center">
-                        <h3>Contact us</h3>
-                    </div>                    
-                </div>
+          {/* Contact */}
+          <div className="footer-column">
+            <i className="fa fa-envelope footer-icon"></i>
 
-                <div className="row margin-bottom-30 footer-contact-social">
-                    <div className="column">
-                            <p className="mouse-link"><i class="fa fa-envelope" style={{marginLeft: '-10px'}}></i></p>  
-                            
-                            <div className="footer-menu">
-                                <h4 className='margin-bottom-26'>Send Message</h4>
-                                <ul className="footer-menu-list">
+            <h4>Send Message</h4>
+            <a href="mailto:example@gmail.com">example@gmail.com</a>
+          </div>
 
-                                    <li className="footer-menu-item">
-                                        <a href="#" className="footer-menu-link">example@gmail.com</a>
-                                    </li>
-                                    
-                                </ul>
-                            </div> 
-                            
-                        </div>                                  
-                                  
-                            
+          {/* Links */}
+          <div className="footer-column">
+            <i className="fa fa-mouse-pointer footer-icon"></i>
 
-                    <div className="column ">
-                        <p className="mouse-link"><i className="fa fa-mouse-pointer " ></i></p>  
-                        
-                        <div className="footer-menu">
-                            <h4 className='margin-bottom-26'>Useful Links</h4>
-                            <ul className="footer-menu-list">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#gallery">Gallery</a></li>
+              <li><a href="#blog">Blog</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
 
-                                <li className="footer-menu-item">
-                                    <a href="#home" className="footer-menu-link">Home</a>
-                                </li>
-                                <li className="footer-menu-item">
-                                    <a href="#about" className="footer-menu-link">About</a>
-                                </li>
-                                <li className="footer-menu-item">
-                                    <a href="#gallery" className="footer-menu-link">Gallery</a>
-                                </li>
-                                <li className="footer-menu-item">
-                                    <a href="#blog" className="footer-menu-link">Blog</a>
-                                </li>
-                                <li className="footer-menu-item">
-                                    <a href="#contact" className="footer-menu-link">Contact</a>
-                                </li>
-                            </ul>
-                        </div> 
-                        
-                    </div>                                  
-                                  
-                            
+          {/* Social */}
+          <div className="footer-column">
+            <i className="fa fa-share-alt footer-icon"></i>
 
-                <div className="column">
-                    <p className="mouse-link"><i class="fa fa-share-alt" style={{marginLeft: '-5px'}}></i></p>  
-
-                    <div className="footer-menu ">
-                        <h4 className="margin-bottom-26">Social Media</h4>
-                        <ul className="footer-menu-list social-media">
-                            <li className="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="fa fa-facebook-official"></i>
-                                </a>
-                            </li>
-                            <li className="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="fa fa-instagram"></i>
-                                </a>
-                            </li>
-                            <li class="footer-menu-item">
-                                <a href="#" class="footer-menu-link">
-                                    <i class="fa fa-youtube-play" style={{}}></i>
-                                </a>
-                            </li>
-                            <li className="footer-menu-item">
-                                <a href="#" className="footer-menu-link">
-                                    <i class="fa fa-pinterest"></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    
-                </div>
-                </div>
-                <div className="row justify-between footer-copyright">
-                    <div className="column ">
-                        <p className="copyright">
-                            © 2022 UsingReact 
-                        </p>
-                    </div>
-                </div>
+            <h4>Social Media</h4>
+            <div className="social-icons">
+              <a href="#"><i className="fa fa-facebook-official"></i></a>
+              <a href="#"><i className="fa fa-instagram"></i></a>
+              <a href="#"><i className="fa fa-youtube-play"></i></a>
+              <a href="#"><i className="fa fa-pinterest"></i></a>
             </div>
+          </div>
+
+        </div>
+
+        <div className="footer-bottom">
+          © 2025 Web Development Engineer
+        </div>
+
+      </div>
     </footer>
-    )
+  );
 }
+
 export default Footer;
